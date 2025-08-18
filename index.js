3b7f01367ae2ac06b7db3e049925169ff06d60e3
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-08-18 18:24:04 +0600

    added all done

--- index.js
+++ index.js
@@ -1,108 +1,103 @@
 // Problem 1: Reverse a String
 
-// function add(name) {
+function add(name) {
+  return name.split("").reverse().join("");
+}
 
-//   return name.split("").reverse().join("");
-
-// }
-
-// console.log(add("hello"));
+console.log(add("hello"));
 
 // Problem 2: Count Vowels in a String
 
-// let myStr = "programming"
+let myStr = "programming";
 
-// let vowels = /[aeiou]/g
+let vowels = /[aeiou]/g;
 
-// let match = myStr.match(vowels).length
-// console.log(match);
+let match = myStr.match(vowels).length;
+console.log(match);
 
 // Problem 3: Check for Palindrome
 
-// let name = "madam";
-// let value = "hello";
+let name = "madam";
+let value = "hello";
 
-// const revValue = () => {
-//   const myRevName = name.split("").reverse().join("");
-//   const checkName = name === myRevName;
-//   console.log("Check Name: ", checkName);
+const revValue = () => {
+  const myRevName = name.split("").reverse().join("");
+  const checkName = name === myRevName;
+  console.log("Check Name: ", checkName);
 
-//   const myRevValue = value.split("").reverse().join("");
-//   const checkValue = value === myRevValue;
-//   console.log("Check Value: ", checkValue);
-// };
+  const myRevValue = value.split("").reverse().join("");
+  const checkValue = value === myRevValue;
+  console.log("Check Value: ", checkValue);
+};
 
-// revValue();
+revValue();
 
 // Problem 4: Find the Maximum Number
 
-// let array = [5, 1, 9, 3];
+let array = [5, 1, 9, 3];
 
-// const bigValue = () => {
-//   const value = Math.max(...array);
-//   console.log(value);
-// };
+const bigValue = () => {
+  const value = Math.max(...array);
+  console.log(value);
+};
 
-// bigValue();
+bigValue();
 
 // Problem 5: Remove Duplicates from an Array
 
-// function uniqueArr(arr) {
-//     let uniqueValue = [... new Set(arr)];
-//     console.log(uniqueValue);
-// }
+function uniqueArr(arr) {
+  let uniqueValue = [...new Set(arr)];
+  console.log(uniqueValue);
+}
 
-// uniqueArr([1, 2, 2, 3, 4, 4]);
+uniqueArr([1, 2, 2, 3, 4, 4]);
 
 // Problem 6: Sum of All Numbers in an Array
 
-// function sum(number) {
-//   let s = 0;
+function sum(number) {
+  let s = 0;
 
-//   for (num of number) {
-//     s += num;
-//   }
+  for (num of number) {
+    s += num;
+  }
 
-//   return s;
+  return s;
+}
 
-// }
-
-// console.log(sum([1, 2, 3, 4]));
+console.log(sum([1, 2, 3, 4]));
 
 // Problem 7: Find Even Numbers in an Array
 
-// function evenNumber(even) {
-//   return even.filter((evenSum) => evenSum % 2 === 0);
-// }
+function evenNumber(even) {
+  return even.filter((evenSum) => evenSum % 2 === 0);
+}
 
-// console.log(evenNumber([1, 2, 3, 4, 5, 6]));
+console.log(evenNumber([1, 2, 3, 4, 5, 6]));
 
 // Problem 8: Capitalize First Letter of Each Word
 
-// function words(word) {
-//   return word
-//     .split(" ")
-//     .map((value) => value[0].toUpperCase() + value.substring(1).toLowerCase())
-//     .join(" ");
-// }
+function words(word) {
+  return word
+    .split(" ")
+    .map((value) => value[0].toUpperCase() + value.substring(1).toLowerCase())
+    .join(" ");
+}
 
-// console.log(words("Hello World"));
+console.log(words("Hello World"));
 
 // Problem 9: Find the Factorial of a Number
-// function value(num){
-//     let fac = 1;
-//     for(let i = num; i >= 1; i--){
-//         fac = fac * i
-//     }
-//     return fac
+function value1(num) {
+  let fac = 1;
+  for (let i = num; i >= 1; i--) {
+    fac = fac * i;
+  }
+  return fac;
+}
 
-// }
-
-// console.log(value(5));
+console.log(value1(5));
 
 // Problem 10: PingPong Challenge
 function PingPong() {
-    
   for (let i = 1; i <= 20; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("PingPong");
