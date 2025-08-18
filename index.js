f23f4dbfec1e61a7f7c267031271786d7f0ff668
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-08-18 16:01:29 +0600

    Find the Maximum Number

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 // Problem 1: Reverse a String
 
-function add(name) {
+// function add(name) {
 
-  return name.split("").reverse().join("");
+//   return name.split("").reverse().join("");
 
-}
+// }
 
-console.log(add("hello"));
-
-
+// console.log(add("hello"));
 
 // Problem 2: Count Vowels in a String
 
-let myStr = "programming"
+// let myStr = "programming"
 
-let vowels = /[aeiou]/g
+// let vowels = /[aeiou]/g
 
-let match = myStr.match(vowels).length
-console.log(match);
-
-
+// let match = myStr.match(vowels).length
+// console.log(match);
 
 // Problem 3: Check for Palindrome
 
-let name = "madam";
-let value = "hello";
+// let name = "madam";
+// let value = "hello";
 
-const revValue = () => {
-  const myRevName = name.split("").reverse().join("");
-  const checkName = name === myRevName;
-  console.log("Check Name: ", checkName);
+// const revValue = () => {
+//   const myRevName = name.split("").reverse().join("");
+//   const checkName = name === myRevName;
+//   console.log("Check Name: ", checkName);
 
-  const myRevValue = value.split("").reverse().join("");
-  const checkValue = value === myRevValue;
-  console.log("Check Value: ", checkValue);
+//   const myRevValue = value.split("").reverse().join("");
+//   const checkValue = value === myRevValue;
+//   console.log("Check Value: ", checkValue);
+// };
+
+// revValue();
+
+// Problem 4: Find the Maximum Number
+
+let array = [5, 1, 9, 3];
+
+const bigNumber = () => {
+    const value = Math.max(...array)
+    console.log(value);
 };
 
-revValue();
+bigNumber();
