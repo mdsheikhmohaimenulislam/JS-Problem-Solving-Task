5cb236c50e54833edfb90a49a2aaa16de2b01260
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-08-18 15:39:55 +0600

    Count Vowels in a String done

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-function add(name) {
+// function add(name) {
 
-  return name.split("").reverse().join("");
+//   return name.split("").reverse().join("");
 
-}
+// }
 
+// console.log(add("hello"));
 
-console.log(add("hello"));
+// let myStr = "programming"
+
+// let vowels = /[aeiou]/g
+
+// let match = myStr.match(vowels).length
+// console.log(match);
+
+let name = "madam";
+let value = "hello";
+
+const revValue = () => {
+  const myRevValue = name.split("").reverse().join("");
+  const checkValue = name === myRevValue;
+  console.log(checkValue);
+};
+
+revValue();
