d8564eab607c27af80d39d0d1eec6f51480708d4
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-09-19 19:49:45 +0600

    chapter_4 complete

---
+++ Chapter_4/day_2.js
@@ -0,0 +1,26 @@
+// function add(name) {
+//   return name.split("").reverse().join("");
+// }
+
+// console.log(add("Hello"));
+
+// function isPalindrome(str) {
+//   const value = str.split("").reverse().join("");
+//   return str == value;
+// }
+
+// console.log(isPalindrome("madam"));
+// console.log(isPalindrome("Hello"));
+
+// function number(arr){
+// const value = Math.max(...arr)
+// return value
+// }
+
+// console.log(number([10, 20, 4, 45, 990]))
+
+// function min(array){
+//   const result = Math.min(...array)
+//   return result
+// }
+// console.log(min([10, 20, 4, 45, 990]))
---
+++ Chapter_4/day_3.js
@@ -0,0 +1,76 @@
+// // 1.
+// console.log(7 > 5);
+
+// // 2.
+// console.log(45 > 50);
+
+// // 3.
+// console.log(75 == 75);
+
+// // 4.
+// console.log(100 == 75);
+
+// // 5.
+// console.log(10 != 7);
+
+// // 6.
+// console.log(20 != 15)
+
+// // 7.
+// console.log(45 != 97)
+
+// console.log(false != false)
+
+// // new 1.
+// console.log(10 >= 15);
+
+// // 2.
+// console.log(33 <= 45);
+
+// // 3.
+// const jambura = 20;
+// const kodbal = 30;
+// console.log(jambura < kodbal);
+
+// // 4.
+// console.log(10 == 10);
+
+// // 5.
+// console.log(15 < 20)
+
+// // 6.
+// console.log(2 <= 3)
+
+// // 7.
+// console.log(true == false)
+
+// console.log(10 === "10")
+
+// console.log(120 === "tomato")
+// console.log(10 != 10)
+
+// console.log(50 == "50");
+// console.log(50 === "50");
+
+// console.log("javaScript" == "javaScript");
+// console.log("javaScript" === "javaScript");
+
+// console.log(25 == "25");
+// console.log(25 === "25");
+
+// console.log("apple" == 'apple')
+// console.log("apple" === "apple")
+
+// console.log("test" == "TEST");
+// console.log("TEST" === "test")
+
+// const variableX = 15;
+// const variableY = "20";
+// console.log(variableX != variableY);
+// console.log(variableX !== variableY);
+
+// const variableA = "hello";
+// const variableB = "Hello";
+// console.log(variableA == variableB);
+// console.log(variableA === variableB)
+
