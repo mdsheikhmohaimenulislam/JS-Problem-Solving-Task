65fbaae3d32ffad53534e6cc732c1a83a683edfe
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-10-05 23:44:18 +0600

    day 4 done

---
+++ Chapter_4/day_4.js
@@ -0,0 +1,71 @@
+// const rainingOutside = true;
+
+// if (rainingOutside == true) {
+//   console.log("You and me under the tree");
+// } else {
+//   console.log("No rain. NO Romance");
+// }
+
+// const weight = 40;
+// if (weight > 20) {
+//   console.log("Rickshaw mama cholo jai.");
+// } else {
+//   console.log("Walking is Exercising");
+// }
+
+//? ..........Practice..........
+
+// 1.
+// const nasta = 5;
+// if(nasta < 6){
+//     console.log("You came home before 6 - you'll get breakfast")
+// }
+// else{
+//     console.log("You're late - You'll get a broom beating!")
+// }
+
+// 2.
+// const login = true;
+// if(login){
+//     console.log("Welcome to login")
+// }
+// else{
+//     console.log("Get lost!")
+// }
+
+
+// 3.
+// const run = 5;
+// if(run == 5){
+//     console.log("You Have a Chocolate")
+// }else{
+//     console.log("You Will Become Fat")
+// }
+
+// 4.
+// const number = 45;
+// if (number >= 40) {
+//     console.log("Exam-e 40 or more pailaso — gari kina dibo!");
+// } else {
+//     console.log("40 er kom paiso — gari dibo na!");
+// }
+
+
+// 5.
+// const time = 10;
+// if(time < 9){
+//     console.log("You came before 9 - Watch the movie!")
+// }
+// else{
+//     console.log("You came after 9 - got to sleep!")
+// }
+
+
+// 6.
+// const Temperature = 40
+// if(Temperature >= 30){
+//     console.log("AC ta on kore dao")
+// }
+// else{
+//     console.log("Sleeping wrapped in a blanket")
+// }
