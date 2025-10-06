2381cc331dc827d27fac28ada9d0e91dabeba572
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-10-06 22:39:51 +0600

    day 5 aktu practice korci

---
+++ Chapter_4/day_5.js
@@ -0,0 +1,17 @@
+// const salary = 63000;
+// const isBCS = true;
+// if (salary > 50000 && isBCS == true) {
+//   console.log("Sei Level er ---- Patro");
+// } else {
+//   console.log("Dore giya moro");
+// }
+
+
+const salary = 25000;
+const height = 68;
+if(salary > 25000 || height > 72){
+    console.log("bolo baba kobul")
+}
+else{
+    console.log("Vag tui mokbul")
+}
