70f8af630767d2b9d9b61e37a2a724092d904218
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-10-07 20:08:11 +0600

    day 5  practice done

--- Chapter_4/day_5.js
+++ Chapter_4/day_5.js
@@ -6,12 +6,96 @@
 //   console.log("Dore giya moro");
 // }
 
+// const salary = 25000;
+// const height = 68;
+// if(salary > 25000 || height > 72){
+//     console.log("bolo baba kobul")
+// }
+// else{
+//     console.log("Vag tui mokbul")
+// }
 
-const salary = 25000;
-const height = 68;
-if(salary > 25000 || height > 72){
-    console.log("bolo baba kobul")
-}
-else{
-    console.log("Vag tui mokbul")
-}
+//? .................Practice....................?//
+
+// 1.
+// const age = 20;
+// const height = 72;
+// if(age > 18 && height > 60){
+//     console.log("You can drive the car")
+// }else{
+//     console.log("SitDown in the car")
+// }
+
+// 2.
+// const mathematicsScore = 80;
+// const englishScore = 70;
+// if(mathematicsScore > 80 || englishScore > 85){
+//     console.log("You can join the class")
+// }else{
+//     console.log("Go Your home and tell Your father i will get married")
+// }
+
+// 3.
+// const studentGPA = 5;
+// const studentSalary = 10000;
+// if(studentGPA >= 5 || studentSalary > 10000){
+//     console.log("You have full Scholarship")
+// }else{
+//     console.log("You don't have any Scholarship")
+// }
+
+// 4.
+// const jobsSeekerAge = 20
+// const jobsExperience = 2;
+// if(jobsSeekerAge < 30 && jobsExperience >= 2){
+//     console.log("You can take the job exam")
+// }else{
+//     console.log("You won't be able to take the job exam")
+// }
+
+// 5.
+// const egg = 13;
+// const hen = false;
+// if(egg > 12 || hen == true){
+//     console.log("egg kurMa ranNa korBo")
+// }else{
+//     console.log("bread or banana eat")
+// }
+
+// 6.
+// const humanBodyTemperature = 105;
+// const cough = false;
+// if (humanBodyTemperature > 100 || cough == true) {
+//   console.log("go to the doctor chamber");
+// } else {
+//   console.log("if not human body temperature 100 then sleep with a blanket");
+// }
+
+// 7.
+// const present = 81;
+// const homeWork = true;
+// if (present > 80 && homeWork == true) {
+//   console.log("You will go to the exam hall.");
+// } else {
+//   console.log(
+//     "You won't present 80% or more, and n't submit homework regularly, then you will auto fail."
+//   );
+// }
+
+// 8.
+// const electricity = true;
+// const phoneCharged = true;
+// if(electricity == true && phoneCharged == true){
+//     console.log("electricity nai porta bosho")
+// }else{
+//     console.log("electricity ace game khalo")
+// }
+
+// 9.
+// const tShirt = 1000;
+// const coupon = true;
+// if(tShirt > 1000 && coupon == true){
+//     console.log("Parcel 20% discount")
+// }else{
+//     console.log("n't available parcel discount")
+// }
