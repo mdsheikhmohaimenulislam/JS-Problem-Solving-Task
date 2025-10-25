81ad56aa1f4d951642d66db08ec73ac4ac09d50f
Md Sheikh Mohaimenul Islam <193258972+mdsheikhmohaimenulislam@users.noreply.github.com>
2025-10-25 20:49:59 +0600

    day 6 added

---
+++ Chapter_4/day_6.js
@@ -0,0 +1,24 @@
+// const result = 40;
+// const ics = false;
+// if (result < 40) {
+//   if (ics === true) {
+//     console.log("ami boladibo toy false korco");
+//   } else if (ics === false) {
+//     console.log("ami kichu bolbona");
+//   }
+// } else if (result >= 40) {
+//   console.log("You are pass");
+// }
+
+const money = 499;
+const popcornPrice = 40;
+
+if(money < 300){
+console.log("gore bosha takbo")
+}else if(money >= 300){
+    if(popcornPrice < 50){
+        console.log("popcorn kinbona")
+    }else if(popcornPrice >= 50){
+        console.log("kinbo")
+    }
+}
