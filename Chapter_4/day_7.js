0d34b97580b14a2e1e0aa739f7e02cf508ab5fb2
Md Sheikh Mohaimenul Islam <193258972+mdsheikhmohaimenulislam@users.noreply.github.com>
2025-10-29 21:55:52 +0600

    day 7 done

---
+++ Chapter_4/day_7.js
@@ -0,0 +1,38 @@
+//? 0.0.
+// const age = 18;
+// age >= 18 ? console.log("Eligible") : console.log("Not Eligible");
+
+// 0.
+// const price = 500;
+// const isLeader = true;
+
+// const finalPrice = isLeader ? 0 : price + 100;
+// console.log(finalPrice);
+
+// 1.
+// const price = 2000;
+// const cashBack = 500;
+
+// const finalPrice = price >= 3000 ? price - cashBack : price;
+// console.log(finalPrice);
+
+// 2.
+// const age = 8;
+
+// const finalAge = age >= 10 ? "Teenager" : "Child";
+// console.log(finalAge);
+
+// 3.
+// const isLoggedIn = false;
+// const message = isLoggedIn ? "Welcome back" : "Please login";
+// console.log(message);
+
+// 4.
+// const oilFull = true;
+// const car = oilFull ? "Ready for a long drive" : "Fill the tank";
+// console.log(car);
+
+// 5.
+// `const examPass = false;
+// const examResult = examPass ? "Party time" : "I will study seriously next semester";
+// console.log(examResult)`
