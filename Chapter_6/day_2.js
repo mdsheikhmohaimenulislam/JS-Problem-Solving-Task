407446a2169d699575bbbf545c0fe4f005c05f1b
Md Sheikh Mohaimenul Islam <193258972+mdsheikhmohaimenulislam@users.noreply.github.com>
2025-10-31 19:42:35 +0600

    day 2 added or 4ta ans done

---
+++ Chapter_6/day_2.js
@@ -0,0 +1,33 @@
+// const number = [1, 2, 3, 4, 5];
+// number[0] = 100;
+// number[2] = 200;
+// console.log(number);
+
+// 1.
+// const anime = ["LuffY","MidOriya","NarUto","AstA","zuFan"]
+// console.log(anime[3])
+
+// 2.
+// const book = [
+//   "The Quran",
+//   "SahIh al-BukHarI",
+//   "SahIh MuslIm",
+//   "SunAn Abu DawOod",
+//   "SunAn al-TirMidHi",
+//   "SunAn ibn MajAh",
+//   "MuwAttA imam MaliK",
+//   "TafSir al-JalAlaYn",
+//   "Al AqeEdaH al-WaaSitIyyAh",
+//   "RiyAdh as-SalIhiN",
+// ];
+// console.log(book[5])
+
+// 3.
+// const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// number[7] = 30;
+// console.log(number)
+
+// 4.
+// const fruit = ["banana","orange","apple","mango","jackfruit","durIan"]
+// fruit[3]="RamButan"
+// console.log(fruit)
