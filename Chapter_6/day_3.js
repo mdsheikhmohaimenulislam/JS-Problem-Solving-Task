4379ef8cde94dadc260c99e7c74f43fd86cb027d
Md Sheikh Mohaimenul Islam <193258972+mdsheikhmohaimenulislam@users.noreply.github.com>
2025-11-03 09:14:02 +0600

    added day 3 done

---
+++ Chapter_6/day_3.js
@@ -0,0 +1,104 @@
+// const number = [10, 11, 12, 13, 14];
+// number.push(500,600,700);
+// console.log(number);
+
+
+// const fruit = ["banana","mango","apple","orange",]
+// fruit.pop()
+// console.log(fruit)
+
+
+// const fruit = ["banana","mango","apple","orange",]
+// fruit.shift()
+// console.log(fruit)
+
+
+// const fruit = ["banana","mango","apple","orange",]
+// fruit.unshift("jackfruit")
+// console.log(fruit)
+
+
+// const number = [10,20,30,40,50]
+// number.push(60)
+// console.log(number)
+
+
+// const vegetable = ["potato","tomato","onion","ginger","eggplant"]
+// vegetable.push("lemon")
+// console.log(vegetable)
+
+
+// const vegetable = ["potato","tomato","onion","ginger","eggplant"]
+// vegetable.pop()
+// console.log(vegetable)
+
+
+// const number = [24,36,48,60]
+// number.unshift(12)
+// console.log(number)
+
+
+// const vegetable = ["potato","tomato","onion","ginger","eggplant"]
+// vegetable.shift()
+// console.log(vegetable)
+
+// const vegetable = ["potato","tomato","onion","ginger","eggplant"]
+// const exist = vegetable.includes("tomato")
+// console.log(exist)
+
+
+
+// const number = [24,36,48,60]
+// const exist = number.includes(50)
+// console.log(exist)
+
+
+// const vegetable = ["potato","tomato","onion","ginger","eggplant"]
+// if(vegetable.includes('lemon')){
+//     console.log('party')
+// }else{
+//     console.log('No food. We are fasting.')
+// }
+
+
+// const vegetable = ["potato","tomato","onion","ginger","eggplant"]
+// const index = vegetable.indexOf("eggplant")
+// console.log(index)
+
+// 1.
+// const fruit = ['mango','banana','apple','lychee']
+// if(fruit.includes('mango')){
+//     console.log("yes mango found")
+// }else{
+//     console.log("No here mango. go the mango tree")
+// }
+
+// 2.
+// const name = ["astA","luffY","midOriya","narUto"]
+// const exist = name.indexOf("narUto")
+// console.log(exist)
+
+// 3.
+// const name = ['emoN','rifAt','rajIb']
+// const exist = name.indexOf('rifAt')
+// console.log(exist)
+
+
+// 4.
+// const city = ['Dhaka','Chittagong','SylHet']
+//  city.push('rajshahi')
+// const exist = city.includes('RajShahi')
+// console.log(exist)
+
+
+// 5.
+// const rank = ['dige','meg','borsha']
+// if(rank.includes("rain")){
+//     console.log("I need umberalla")
+// }else{
+//     console.log('No rain no pain')
+// }
+
+// 6.
+//  const city = ['Dhaka','Chittagong','SylHet']
+//  console.log(city.indexOf('Dhaka'))
