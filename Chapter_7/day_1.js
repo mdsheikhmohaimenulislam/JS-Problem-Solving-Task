97b51429bd7c946f49c3657b2c89fce25750eab6
Md Sheikh Mohaimenul Islam <193258972+mdsheikhmohaimenulislam@users.noreply.github.com>
2025-11-07 16:41:31 +0600

    chapter 7 or day 1 done

---
+++ Chapter_7/day_1.js
@@ -0,0 +1,32 @@
+// const number = [1, 2, 3, 44, 55, 6, 65, 5, 55, 11];
+// for (const num of number) {
+//   console.log(num);
+// }
+
+// const fruits = ["orange", "apple", "banana", "jackfruit", "watermelon"];
+// for (const items of fruits) {
+//   console.log(items);
+// }
+
+// const name = ["LuffY","AstA","MidOriya","BhaI hoo","le fan"];
+// for(const items of name){
+//     console.log(items)
+// }
+
+// const book = [
+//   "clarify",
+//   "define",
+//   "consent",
+//   "process",
+//   "pursue",
+//   "demonstrate",
+// ];
+// for (const bookItems of book) {
+//   console.log(bookItems);
+// }
+
+
+// const anime = ["one pice","hero academic","black cover","book of espEr"]
+// for(const items of anime){
+//     console.log(items)
+// }
