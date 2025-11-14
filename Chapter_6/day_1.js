0da0574dfdf3cc0bfebab680845efe5eba0ce774
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-11-14 22:44:14 +0600

    Uncomment fruit and anime arrays
    Uncomment fruit and anime arrays for logging.

--- Chapter_6/day_1.js
+++ Chapter_6/day_1.js
@@ -1,8 +1,8 @@
 // const numbers = [71,72,73,74,75,76]
 // console.log(numbers.length)
 
-// const fruit = ["apple", "banana", "orange", "mango","guava"];
-// console.log(fruit)
+// const fruit = ["apple", "banana", "orange", "mango", "guava"];
+// console.log(fruit) 
 
 // const vegetable = [
 //   "potato",
@@ -19,7 +19,7 @@
 // ];
 // console.log(vegetable.length)
 
-// const anime = ["onePice", "naRuTo", "book of esPer", "my hero academia", "black clover"]
+// const anime = ["onePice", "naRuTo", "book of esPer", "my hero academia", "black clover"] 
 // console.log(anime.length)
 
 // const number = [11,13,15,17,19,21,23,25,27,29]
