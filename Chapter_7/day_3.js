04a8ab089a0f4332451dabe7fbef02f77fae7a9a
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-11-27 22:08:13 +0600

    practice 2 done

---
+++ Chapter_7/day_3.js
@@ -0,0 +1,13 @@
+// 1
+// let num = 1;
+// while(num <= 10){
+//     console.log("I am write 3 hour code ");
+//     num++;
+// }
+
+// 2
+// let num = 1;
+// while (num <= 20) {
+//   console.log(num);
+//   num++;
+// }
