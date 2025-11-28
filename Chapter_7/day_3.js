ada6e18a0f64587fc35a994a24eae42a5498b5c7
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-11-28 16:20:43 +0600

    ses while loop

--- Chapter_7/day_3.js
+++ Chapter_7/day_3.js
@@ -11,3 +11,72 @@
 //   console.log(num);
 //   num++;
 // }
+
+// 3
+// let num = 50;
+// while (num <= 100) {
+//   console.log(num);
+//   num++;
+// }
+
+// 4
+// let num = 1
+// while(num <=10){
+//     console.log(num * 3)
+//     num++
+// }
+
+// 5
+// let num = 111
+// while(num >= 110){
+//     console.log(num / 2)
+//     num--
+// }
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//   console.log(num);
+//   sum = sum + num
+//   num++;
+// }
+// console.log("Sum: ",sum)
+
+// let num = 50;
+// while (num <= 100) {
+//   console.log(num);
+//   num++;
+// }
+
+// let num = 5;
+// let sum = 0;
+// while (num <= 15) {
+//   sum = sum + num;
+//   num++;
+// }
+// console.log(sum);
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 50) {
+//   sum = sum + num;
+//   num++;
+// }
+// console.log(sum);
+
+// let num = 21;
+// let sum = 0;
+// while (num <= 50) {
+//   sum = sum + num;
+//   console.log(sum);
+//   num++;
+// }
+
+
+// let num = 20;
+// let sum = 0;
+// while(num <= 40){
+//     sum = sum + num;
+//     num++
+// }
+// console.log(sum)
