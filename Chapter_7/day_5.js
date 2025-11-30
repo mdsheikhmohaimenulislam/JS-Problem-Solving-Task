76b0bca03089696032b4e9938db9310d80818b10
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-11-30 15:03:39 +0600

    added for loop practice done

---
+++ Chapter_7/day_5.js
@@ -0,0 +1,80 @@
+// !.....................Loop Break...............
+// for(let i = 1;i<= 15;i++){
+//     if(i >= 7){
+//         console.log("1 to 7:",i)
+//         break
+//     }
+
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i == 6 || i==7 || i ==10|| i ==11) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 1) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// ?.........................Practice............................
+// 1
+// for (let i = 1; i <= 30; i++) {
+//   if (i > 15) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// 2
+// for (let i = 1; i <= 40; i++) {
+//   if (i % 7 == 0) {
+//     continue;
+//   }
+//       console.log(i);
+
+// }
+
+// 3
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 9) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// 4
+// for(let i = 1;i <= 20;i++){
+//     if( i == 12){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// 5
+// for (let i = 1; i <= 25; i++) {
+//   if (i % 3 == 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// 6
+// for (let i = 91; i <= 120; i++) {
+//   if (i % 10 == 0) {
+//     console.log("A number divisible by 10 has been found:", i);
+//     break;
+//   }
+//   console.log(i);
+// }
