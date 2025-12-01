2579d47022ec689a081172ce8fbb9daa9f30e090
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-12-01 11:18:40 +0600

    added chapter 8

---
+++ Chapter_8/day_1.js
@@ -0,0 +1,42 @@
+// const subject = {
+//   name: "biology",
+//   teacher: "Loi",
+//   examDate: "7 dec",
+//   chapters: ["first", "second", "third"],
+//   nextExam: {
+//     name: "Final Exam",
+//     marks: 100,
+//   },
+// };
+// console.log(subject.chapters);
+
+// 1. Practice
+// const teacher = {
+//     name:"Loi",
+//     age:"25",
+//     homeLocation:"UK",
+//     subject:"Mathematics",
+//     weight:"45 kg"
+// }
+
+// 2.
+// const tree = {
+//   name: "mangoTree",
+//   fruitName: "mango",
+//   hight: 60,
+//   age: 120,
+// };
+
+// 4
+// const loi = {
+//   name: "Loi",
+//   age: 25,
+//   hight: 6,
+//   weight: 50,
+//   child: ["loi1", "loi2", "loi3"],
+//   job: {
+//     subject: "mathematics",
+//     student: 40,
+//     className: "LoiHOme",
+//   },
+// };
