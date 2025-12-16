5aae29ed93a4076cb85b0349f366a76e52b7a663
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-12-16 21:27:25 +0600

    Update day_3.js

--- Chapter_7/day_3.js
+++ Chapter_7/day_3.js
@@ -1,32 +1,32 @@
-// 1
+// 1......................
 // let num = 1;
 // while(num <= 10){
 //     console.log("I am write 3 hour code ");
 //     num++;
 // }
 
-// 2
+// 2...................
 // let num = 1;
 // while (num <= 20) {
 //   console.log(num);
 //   num++;
 // }
 
-// 3
+// 3........................
 // let num = 50;
 // while (num <= 100) {
 //   console.log(num);
 //   num++;
 // }
 
-// 4
+// 4...............
 // let num = 1
 // while(num <=10){
 //     console.log(num * 3)
 //     num++
 // }
 
-// 5
+// 5..................
 // let num = 111
 // while(num >= 110){
 //     console.log(num / 2)
