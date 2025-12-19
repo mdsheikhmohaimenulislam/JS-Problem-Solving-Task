8d6dcc28645a2a1a64f82e9db905c3b4608f932d
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-12-19 11:53:48 +0600

    BracketNotation some value added

---
+++ Chapter_8/day_3.js
@@ -0,0 +1,82 @@
+//!...................BracketNotation...............
+
+// const person = {
+//     profession:"developer",
+//     age:12
+// }
+
+// const myAge = "age"
+// console.log(person[myAge])
+
+// const propertyName = "profession";
+// console.log(person[propertyName])
+
+// const myDream ={
+//     name: "loi",
+//     age:19,
+//     profession:"developer",
+// };
+
+// // console.log(myDream)
+
+// const myName = 'name'
+// console.log(myDream[myName])
+// console.log(myDream.name)
+// console.log(myDream['name'])
+
+// myDream.name = "MohAi"
+// console.log(myDream['name'])
+
+// myDream['age'] = 20
+// console.log(myDream.age)
+
+// const animeName = {
+//     DragonBallZ:"Son",
+//     Naruto: "Naruto",
+//     onePiece: "luffy",
+//     AttackonTitan: "Eren",
+//     hunter:"gon"
+// }
+
+// console.log(animeName)
+// console.log(animeName.Naruto)
+// console.log(animeName['Naruto'])
+
+// animeName.DragonBallZ = "guku"
+// console.log(animeName.DragonBallZ)
+
+// animeName['onePiece'] = 'D.Luffy'
+// console.log(animeName['onePiece'])
+
+// const name = {
+//   name: "loi",
+//   anime: {
+//     Naruto: "Naruto",
+//     onePiece: "luffy",
+//   },
+// };
+// console.log(name['anime'])
+// name.anime.Naruto = "Naruto2"
+// console.log(name.anime.Naruto)
+
+// name['anime']['onePiece'] = "D.luffy"
+// console.log(name['anime']['onePiece'])
+
+// const college = {
+//   name: "loi",
+//   class: [11, 12],
+//   events: ["science fair", "bijoy dibos", "21 feb"],
+//   unique: {
+//     color: "blue",
+//     result: {
+//       gpa: 5,
+//       merit: "top",
+//     },
+//   },
+// };
+// console.log(college.unique.result.merit)
+// college.unique.result.gpa = 4;
+// console.log(college.unique.result.gpa);
+
+// college["unique"]['result']['merit'] = "second";
+// console.log(college["unique"]["result"]["merit"])
