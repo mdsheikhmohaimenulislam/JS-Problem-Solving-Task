df73fdbbcd219b55109df4f087a217eb333b32d9
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-12-26 08:28:23 +0600

    Practice done

---
+++ Chapter_8/day_4.js
@@ -0,0 +1,72 @@
+// const player = {
+//   name: "loi",
+//   team: "the true",
+//   age: 20,
+//   result: {
+//     value: 20,
+//     win: "the true 1",
+//     member: 20,
+//   },
+// };
+
+// console.log(player.name)
+
+// const laptop = {
+//   brand: "Apple",
+//   price: 150000,
+//   hardDisc: "1tb",
+//   ram: "520 gb",
+//   screenSize: "15 inc",
+// };
+
+// console.log(laptop.screenSize)
+
+// const favPlace = {
+//   name: "Cox's Bazar",
+//   distance: "400km",
+//   popularity: "high",
+// };
+
+// console.log(favPlace['popularity'])
+
+// const phone = {
+//   brand: "loi",
+//   price: 2300000,
+//   color: "blue",
+// };
+
+// console.log(phone['price'])
+
+// const library = {
+//   name: "Public Library",
+//   location: "Dhaka",
+//   book: 500,
+// };
+
+// console.log(library.location)
+
+// const college = {
+//   name: "Loi Academic",
+//   established: 2025,
+//   group: ["Science", "Arts", "Commerce"],
+// };
+
+// console.log(college.group[1])
+
+// const animeFamily = {
+//   father: {
+//     name: "Dragon",
+//     age: 25,
+//     profession: "Supreme Commander of the Revolutionary Army",
+//   },
+//   mother: {
+//     name: "Toritoma",
+//     age: 20,
+//   },
+// };
+
+// const sum = animeFamily.father.age + animeFamily.mother.age;
+
+// console.log(sum);
+
+
