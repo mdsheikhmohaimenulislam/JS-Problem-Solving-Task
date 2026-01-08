164fa2327463f101ba5835c4ae98e1d01e9d750e
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-01-08 07:55:07 +0600

    practice 3  done

---
+++ Chapter_8/day_5.js
@@ -0,0 +1,72 @@
+// const person = {
+//   name: "Alice",
+//   age: 20,
+//   country: "JPN",
+// };
+
+// delete person.age;
+// delete person.country
+// console.log(person)
+
+// const adminUser = {
+//   userName: "admin",
+//   email: "admin@gmail.com",
+//   role: "FNd",
+// };
+// Object.freeze(adminUser);
+
+// adminUser.role = "user";
+// adminUser.password = 1234;
+// delete adminUser.role
+// console.log(adminUser)
+
+// const user = {
+//   userName: "loi",
+//   email: "loi@gmail.com",
+//   password: 1234,
+// };
+// Object.seal(user);
+
+// user.password = 123456;
+// console.log(user);
+
+//!...............Practice.....................!//
+// 1
+// const headPhone = {
+//   brand: "Loi",
+//   price: 3000,
+//   color: "blue",
+// };
+// Object.freeze(headPhone);
+
+// headPhone.price = 3500;
+// headPhone.new = "model";
+// console.log(headPhone);
+
+// 2
+// const player = {
+//   name: "Loi",
+//   goals: 3,
+//   club: "miami",
+// };
+// Object.freeze(player)
+
+// player.sumName = "Loi3"
+// console.log(player)
+
+// 3
+// const book = {
+//   title: "Harry Potter",
+//   author: "Jk Rowling",
+//   page: 850,
+// };
+// Object.seal(book);
+
+// book.author = "Loi";
+// book.page = 950;
+// book.color = "Blue"; // new add hobE na
+// delete book.page; // delete o hobE na
+// console.log(book);
+
+// 4
+
