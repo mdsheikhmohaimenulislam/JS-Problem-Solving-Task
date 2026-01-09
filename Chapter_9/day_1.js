8a3cdf30cd57ff09a365927e7b1bc5c8f1f9ce82
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-01-09 07:38:54 +0600

    create chapter 9 done & some question solve done

---
+++ Chapter_9/day_1.js
@@ -0,0 +1,64 @@
+// function jusKHaw() {
+//   console.log("ai now lemon jus!");
+// }
+// jusKHaw();
+
+// 2
+// function myRoutine() {
+//   console.log("face wash");
+//   console.log("Wearing warm clothes");
+//   console.log("Light breakfast");
+
+//   console.log("breakfast");
+// }
+// myRoutine();
+
+// 3
+// function memBer() {
+//   console.log("GrandFather");
+//   console.log("GrandMother");
+//   console.log("NanA");
+//   console.log("Granny");
+//   console.log("Father");
+//   console.log("Mother");
+//   console.log("Mother in law");
+//   console.log("Father in law");
+//   console.log("Sister in law");
+//   console.log("Big Sister");
+//   console.log("Mezzo Sister");
+//   console.log("And ME!");
+// }
+// memBer()
+
+// 4
+// function taskDone() {
+//   console.log("Eating food");
+//   console.log("Reading a book");
+//   console.log("Coding");
+//   console.log("Watching prodCas");
+// }
+// taskDone();
+
+// 5
+// function greetMe() {
+//   console.log("Good Morning! Have a great day!");
+// }
+// greetMe()
+
+// 6
+// function getFavAnime() {
+//   console.log("NarUto");
+//   console.log("One pice");
+//   console.log("Pokemon");
+// }
+// getFavAnime()
+
+// 7
+// function introduceMyself() {
+//     console.log("loi")
+//     console.log("20")
+//     console.log("JP")
+//     console.log(7799)
+//     console.log("Apple")
+// }
+// introduceMyself()
