b773fd17f307f814d92b87316c05a87e387ab1ef
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-01-16 09:02:50 +0600

    practice day 2 done

--- Chapter_9/day_2.js
+++ Chapter_9/day_2.js
@@ -11,9 +11,69 @@
 // const result = addNumbers(5, 5, 5);
 // console.log(result);
 
-function addNumbers(a, b) {
-  console.log(arguments);
-  return a + b;
-}
-const result = addNumbers(5,8,9,6)
-console.log(result)
+// function addNumbers(a, b) {
+//   console.log(arguments);
+//   return a + b;
+// }
+// const result = addNumbers(5,8,9,6)
+// console.log(result)
+
+// 1
+// function sum(son, father) {
+//   return son + father;
+// }
+// const result = sum(12, 24);
+// console.log(result);
+
+// 2
+// function add(a, b) {
+//   const sum = a * b;
+//   console.log(sum);
+// }
+// add(20, 15);
+
+// 3
+// function exam(a, b, c) {
+//   const result = a + b + c;
+//   console.log(result);
+// }
+// exam(80, 78, 90);
+
+// 4
+// function age(oldYear, newYear) {
+//   return newYear - oldYear;
+// }
+// const result = age(2008, 2026);
+// console.log(result);
+
+// 5
+// function vag(law){
+//   const result = law / 2
+//   console.log(result)
+// }
+// vag(175)
+
+// 6
+// function gor(a,b,c,d){
+//   return a+b+c+d
+// }
+// const result = gor(20,25,25,20)
+// console.log(result)
+
+// 7
+// function sellingPrice(costPrice) {
+//   return costPrice + 250;
+// }
+// console.log(sellingPrice(25))
+
+// 8
+// function age(birth) {
+//   return birth + 100;
+// }
+// console.log(age(2005));
+
+// 9
+// function wasteInMonth(wasteTime) {
+//   return wasteTime * 30;
+// }
+// console.log(wasteInMonth(1));
