94cab2c987e65bdca8b85cb5ade0c5873a068847
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-01-28 21:38:53 +0600

    practice day 2 done

--- Chapter_9/day_2.js
+++ Chapter_9/day_2.js
@@ -39,15 +39,14 @@
 // }
 // exam(80, 78, 90);
 
-// 4.......................000dsssdd0.......dddd......dddd............
+// 4................................................
 // function age(oldYear, newYear) {
 //   return newYear - oldYear;
 // }
 // const result = age(2008, 2026);
 // console.log(result);
 
-// 5..........................000dddddd0.ddd.......dddd......
-// function vag(law){
+// 5...............................
 //   const result = law / 2
 //   console.log(result)
 // }
