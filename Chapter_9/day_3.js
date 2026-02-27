1cec93b8a5d73c7d4f326f4fa2dfa5d1a3406d79
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-02-27 19:49:25 +0600

    chapter 9 ar day 3 done

---
+++ Chapter_9/day_3.js
@@ -0,0 +1,61 @@
+// function tenTimes(number){
+//     const result = number * 10;
+//     return result
+// }
+// const total = tenTimes(5);
+// console.log(total)
+
+// function add(price1, price2) {
+//   const total = price1 + price2;
+//   return total;
+// }
+// const bill = add(5, 80);
+// console.log(bill);
+
+// function add(price1, price2) {
+//   return price1 + price2;
+// }
+// const bill = add(5, 80);
+// console.log(bill);
+
+// const potato = 35;
+// const onion = 62;
+// const shirt = 500;
+// const pant = 1250;
+
+// function add(price1, price2) {
+//   return price1 + price2;
+// }
+// const bill1 = add(potato, onion);
+// const bill2 = add(shirt, pant);
+// const final = add(bill1, bill2);
+// console.log(final)
+
+// function doMath(num1, num2) {
+//   const sum = num1 + num2;
+//   const diff = num1 - num2;
+//   const multiply = sum * diff;
+//   const result = multiply / 2;
+//   return result;
+// }
+// const result = doMath(10,5);
+// console.log(result)
+
+// function isEven(num) {
+//   if (num % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(isEven(5))
+// console.log(isEven(10))
+
+// function isOdd(num) {
+//   if (num % 2 === 1) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isOdd(10))
+// console.log(isOdd(9))
