2b99611f2da2efd1fc7c0d3ff96d01172481abfb
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-04 19:28:44 +0600

    create typeScript file and 2 task done

---
+++ TypeScript/src/Practice/type.ts
@@ -0,0 +1,12 @@
+//? Task 1: The "Optional" Shopping Cart
+
+type CarItem = {
+  name: string;
+  price: number;
+  quantity?: number;
+};
+
+
+function calculateTotal (){
+
+}
---
+++ TypeScript/src/text.ts
@@ -0,0 +1,94 @@
+// let bazarList: (string | number)[] = ["eggs",11,"milk","sugar"];
+
+// bazarList.push("apple")
+
+// let coordinates: [number,number,number] = [100,1000,500];
+
+// const user: {
+//   firstName?: string;
+//   middleName: string;
+//   lastName?: string;
+// } = {
+// firstName:"k",
+//   middleName: "loi",
+
+// };
+
+// const user: {
+//   readonly firstName?: string;
+//   middleName: string;
+//   lastName?: string;
+// } = {
+//   firstName: "k",
+//   middleName: "loi",
+// };
+
+// function add(num:number, num2:number){
+//     return num + num2;
+// }
+
+// console.log(add(44,55));
+
+// const add = (num1: number, num2: number) => num1 + num2;
+
+// add(55, 66);
+
+// object => function => method
+const car = {
+  name: "loi",
+  prince: 40,
+  addNewPrice(value: number): number {
+    const totalPrice = this.prince + value;
+    return totalPrice;
+  },
+};
+// console.log(car.addNewPrice(5000));
+
+const arr: number[] = [1, 2, 3, 4];
+
+const squareArray = arr.map((elem: number): number => elem * elem);
+
+// console.log(squareArray);
+
+// Object destructuring array
+
+// const user = {
+//   id: 12,
+//   name: {
+//     firstName: "loi",
+//   },
+//   gender: "male",
+//   color: "blue",
+// };
+
+// type CartItem = {
+//   name: string;
+//   price: number;
+//   quantity?: number;
+// };
+
+// function calculateTotal(item: CartItem) {
+//   const { price, quantity = 1 } = item;
+//   //   if (item?.quantity === 0) {
+//   //     const quantity = 1;
+//   //     `${item.name} ${item.price * quantity}`
+//   //   }
+
+//   return `name: ${item.name} price: ${price * quantity}`;
+// }
+
+// console.log(calculateTotal({ name: "pencil", price: 100 }));
+
+// Task 2: Merging User Profiles
+
+type Person = { name: string; age: number };
+type JobDetails = { role: string; salary: number };
+
+type Employee = Person & JobDetails;
+
+function getProfile(employee: Employee) {
+  const { name, role } = employee;
+  return `name: ${name}  role: ${role} `;
+}
+
+console.log(getProfile({ name: "loi", role: "dev", age: 22, salary: 500 }));
