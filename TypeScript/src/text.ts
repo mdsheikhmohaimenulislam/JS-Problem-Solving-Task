5900b5fb584179ff7190eab03ea6f13ed5c0081f
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-05 13:24:20 +0600

    Task 10: Utility Type (Omit) completed

--- TypeScript/src/text.ts
+++ TypeScript/src/text.ts
@@ -170,20 +170,33 @@ const result2: ReadonlyAndOptional<MyDocument> = {
 };
 // console.log(result2);
 
-
 //! Task 9: The Wrapper
-type Data = number
+type Data = number;
 
-type DataType<T> = T extends any[]?"Large":"Small";
-const value:DataType<Data> = "Small";
+type DataType<T> = T extends any[] ? "Large" : "Small";
+const value: DataType<Data> = "Small";
 
 // console.log(value);
 
 // !
 
-function getData(input:any | any[]){
-  return Array.isArray(input)? "Large":"Small"
+function getData(input: any | any[]) {
+  return Array.isArray(input) ? "Large" : "Small";
 }
 
 // console.log(getData([33,33]));
-//! 
+//!
+
+// Task 10: Utility Type (Omit)
+interface UserAccount {
+  id: number;
+  username: string;
+  password: string;
+}
+
+type PublicUser = Omit<UserAccount, "password">;
+const userProfile: PublicUser = {
+  id: 3,
+  username: "oi",
+};
+console.log(userProfile);
