d737249a8d9f9c51da21f3c970d328b5106e0eff
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-06 12:38:42 +0600

    in guard  done

--- TypeScript/src/Practice/text1.ts
+++ TypeScript/src/Practice/text1.ts
@@ -38,3 +38,36 @@ const student1 = new Student("Shafi", "Shodor", 25);
 
 const teacher1 = new Teacher("sence", "shodor", 25, "junior teacher");
 // teacher1.takeClass(6);
+
+// Type guard using typeof and in
+type Alphaneumeric = number | string;
+
+const add = (num: Alphaneumeric, num2: Alphaneumeric) => {
+  if (typeof num === "number" && typeof num2 === "number") {
+    return num + num2;
+  } else {
+    return num.toString() + num2.toString();
+  }
+};
+
+// console.log(add("2", "50"));
+
+// in guard
+type NormalUser = {
+  name: string;
+};
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`This name ${user.name} and his role id ${user.role}`);
+  } else {
+    console.log(` His name: ${user.name} `);
+  }
+};
+
+// getUserInfo({name:"ruhi",role:"Admin"})
+
