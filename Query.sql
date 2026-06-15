3070bb5064311c0a308a117af956107da3ee6698
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-15 23:36:21 +0600

    Add query to fetch top 2 expensive matches excluding highest

--- Query.sql
+++ Query.sql
@@ -12,6 +12,7 @@ CREATE TABLE Users (
    check(role in ('Ticket Manager','Football Fan'))
 );
 
+
 CREATE TABLE Matches (
     match_id serial primary key,
     fixture varchar(300) not null,
@@ -21,6 +22,7 @@ CREATE TABLE Matches (
 
     check(match_status in ('Available','Selling Fast','Sold Out','Postponed'))
 );
+
 
 CREATE TABLE Bookings (
     booking_id serial primary key,
@@ -36,11 +38,13 @@ CREATE TABLE Bookings (
 );
 
 
+-- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
 
 INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
 (101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
@@ -49,6 +53,7 @@ INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, 
 (104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
 (105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
 
+
 INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
 (501, 1, 101, 'A-12', 'Confirmed', 150.00),
 (502, 1, 102, 'B-04', 'Confirmed', 120.00),
@@ -56,6 +61,45 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
+
+-- 1...
 select match_id, fixture, round(base_ticket_price) as base_ticket_price
 from matches
 where match_status = 'Available' and tournament_category = 'Champions League'
+
+
+-- 2.............
+select user_id, full_name, email from users
+where full_name ilike 'Tanvir%'
+or full_name ilike '%Haque%'
+
+
+-- 3......
+select booking_id,user_id,match_id,
+  coalesce(payment_status, 'Action Required') as systematic_status
+from bookings
+where payment_status is null;
+
+
+-- 4......
+select booking_id, full_name, fixture, 
+  round(total_cost) as total_cost
+  from bookings as b
+inner join users as s on b.user_id= s.user_id
+inner join matches as m on  b.match_id = m.match_id
+
+-- 5.....
+select u.user_id, u.full_name, b.booking_id from users as u
+left join bookings as b on b.user_id = u.user_id;
+
+
+-- 6....
+select booking_id, match_id, round(total_cost) as total_cost from bookings
+where total_cost > (select avg(total_cost) from bookings)
+
+
+-- 7...........
+select match_id, fixture,
+  round(base_ticket_price) as base_ticket_price
+  from matches
+order by base_ticket_price desc limit 2 offset 1;
