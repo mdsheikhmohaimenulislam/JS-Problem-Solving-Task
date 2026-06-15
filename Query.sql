9ce2a8ad7a1dc47701904db3a8e7be948e01c97a
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-15 23:29:22 +0600

    insert into users table data added

--- Query.sql
+++ Query.sql
@@ -34,3 +34,10 @@ CREATE TABLE Bookings (
     check(payment_status in ('Pending','Confirmed','Cancelled','Refunded')),
     check(total_cost >= 0) 
 );
+
+
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
