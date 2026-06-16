ff71730b1df58a863373ecb7db3d2228ac8df315
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-16 22:20:08 +0600

    Add query to working employess

---
+++ work.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE WorkIng
+CREATE TABLE employees (
+  employee_id SERIAL PRIMARY KEY,
+  employee_name varchar(50),
+  department_id int REFERENCES departments (department_id),
+  salary decimal(10, 2),
+  hire_date DATE
+)
