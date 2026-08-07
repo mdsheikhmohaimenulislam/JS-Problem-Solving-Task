65468dc7800b9a65aa020b423ae56f3bddb44d3b
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-08-07 23:23:26 +0600

    fix logout action and remove auth cookies

--- TypeScript/src/Practice/text2.ts
+++ TypeScript/src/Practice/text2.ts
@@ -143,7 +143,7 @@ const shape3 = new Rectangle(10, 20);
 //     console.log(`Pause music...............`);
 //   }
 //   stop() {
-//     console.log(`Stop music...............`);
+//     console.log(`Stop music............5...`);
 //   }
 // }
 
